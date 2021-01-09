03258ccb01d7cd01867c9d6c15f30e1650775d3c
erick4556 <electronicmade9064@outlook.com>
2021-01-09 12:35:33 -0500

    Delete and refresh function for posts

--- screens/AddPostScreen.js
+++ screens/AddPostScreen.js
@@ -79,22 +79,12 @@ const AddPostScreen = () => {
   };
 
   const uploadImage = async () => {
+    if (image === null) {
+      return null;
+    }
+
     const uploadUri = image;
     let filename = uploadUri.substring(uploadUri.lastIndexOf('/') + 1); //File name
-
-    /* setUploading(true);
-    try {
-      await storage().ref(filename).putFile(uploadUri);
-      Alert.alert(
-        'Image uploaded!',
-        'Your image has been uploaded to the Firebase Cloud Storage Successfully!',
-      );
-      setUploading(false);
-    } catch (error) {
-      console.log(error); 
-    } 
-
-    setImage(null);*/
 
     // Add timestamp to File Name
     const extension = filename.split('.').pop();
@@ -125,14 +115,13 @@ const AddPostScreen = () => {
       const url = await storageRef.getDownloadURL(); //Para recibir el enlace de la imagen
 
       setUploading(false);
+      setImage(null);
 
       return url;
     } catch (e) {
       console.log(e);
       return null;
     }
-
-    setImage(null);
   };
 
   return (
